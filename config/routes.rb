61d28b2546c9dfa8ec5d39663a3f58872f7cd1b2
mhollow56 <mikeholloway7023@protonmail.com>
2023-02-02 20:06:42 -0600

    create routes

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'pages#home'
+  get '/about', to: 'pages#about'
+  get '/ terms', to: 'pages#terms'
+
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
